727abe9e11df04c89d64d88f00ad72edb2351e23
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-12-14 21:50:08 +0500

    added more services

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -5,16 +5,14 @@ export default function ServiceCard({service}) {
     <div className="bg-white shadow rounded p-4">
       <img
         src={service.image}
-        className="rounded mb-2"
+        alt={service.title}
+        className="h-48 w-full object-cover rounded mb-3"
       />
       <h3 className="font-bold">{service.title}</h3>
       <p>${service.price}</p>
       <p>⭐ {service.rating}</p>
-      <Link
-        to={`/service/${service.id}`}
-        className="text-blue-500 underline"
-      >
-        View Details
+      <Link to={`/service/${service.id}`}>
+      <button>View Details</button>
       </Link>
     </div>
   );
